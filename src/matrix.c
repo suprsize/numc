6e32650366cc905e4a65844005e2b55142bc1380
Mohammad macbook <hello@MacBook-Pro.local>
2021-08-09 00:19:59 -0700

    OMP for mul

--- src/matrix.c
+++ src/matrix.c
@@ -218,11 +218,11 @@ int mul_matrix(matrix *result, matrix *mat1, matrix *mat2) {
         for(int c = 0; c < result->cols; c++) {
             double temp_sum = 0;
             int size = mat1->cols;
-            #pragma omp parallel
+//            #pragma omp parallel
             {
                 __m256d temp1, temp2;
                 __m256d sum = _mm256_setzero_pd();
-                #pragma omp for
+//                #pragma omp for
                 for(int k = 0; k < size / 4 * 4; k += 4) {
                     temp1 = _mm256_loadu_pd(mat1->data + (mat1->cols * r + k));
                     temp2 = _mm256_loadu_pd(transp2->data + (transp2->cols * c + k));
@@ -231,7 +231,7 @@ int mul_matrix(matrix *result, matrix *mat1, matrix *mat2) {
                 double sum_arr[4];
                 _mm256_storeu_pd(sum_arr, sum);
                 double arr_total = sum_arr[0] + sum_arr[1] + sum_arr[2] + sum_arr[3];
-                #pragma omp critical
+//                #pragma omp critical
                 temp_sum += arr_total;
             }
             for(int k = size - (size % 4); k < size; k++) {

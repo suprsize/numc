2121d871263d53861a0b1d5b80b4f781b8c5452c
Mohammad macbook <hello@MacBook-Pro.local>
2021-08-08 23:32:00 -0700

    OMP for add

--- src/matrix.c
+++ src/matrix.c
@@ -165,6 +165,7 @@ int add_matrix(matrix *result, matrix *mat1, matrix *mat2) {
     {
         __m256d sum, temp1, temp2;
         #pragma omp for
+        printf("Number of threads: %d", omp_get_num_threads());
         for(unsigned int i = 0; i < size / 4 * 4; i += 4) {
             temp1 = _mm256_loadu_pd(mat1->data + i);
             temp2 = _mm256_loadu_pd(mat2->data + i);
